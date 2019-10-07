d56b83af983a55a0a52e2c446002c3f48a9b8694
Divyanka Bose <dibose@microsoft.com>
2019-10-07 16:35:35 -0700

    implementation

--- Practice/src/main/scala/test.sc
+++
@@ -1,7 +0,0 @@
-import testList
-
-def nth[T](n: Int, l: List[T]): T = {
-  if (l.isEmpty) throw new IndexOutOfBoundsException("")
-  else if (n == 0) l.head
-  else nth(n-1, l.tail)
-}
--- Week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ Week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -38,34 +38,36 @@ abstract class TweetSet {
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
    *
-   * Question: Can we implment this method here, or should it remain abstract
+   * Question: Can we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
-   * This is a helper method for `filter` that propagetes the accumulated tweets.
+   * This is a helper method for `filter` that propagates the accumulated tweets.
    */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
   /**
    * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
    *
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
-  
+    def union(that: TweetSet): TweetSet
+
+    def isEmpty: Boolean
+
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
    * Calling `mostRetweeted` on an empty set should throw an exception of
    * type `java.util.NoSuchElementException`.
    *
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def mostRetweeted: Tweet = ???
+    def mostRetweeted: Tweet
   
   /**
    * Returns a list containing all tweets of this set, sorted by retweet count
@@ -73,10 +75,10 @@ abstract class TweetSet {
    * have the highest retweet count.
    *
    * Hint: the method `remove` on TweetSet will be very useful.
-   * Question: Should we implment this method here, or should it remain abstract
+   * Question: Should we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def descendingByRetweet: TweetList = ???
+    def descendingByRetweet: TweetList
   
   /**
    * The following methods are already implemented
@@ -107,8 +109,16 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
+    def union(that: TweetSet): TweetSet = that
+
+    def isEmpty: Boolean = true
+
+    def mostRetweeted : Tweet = Nil.head
+
+    def descendingByRetweet(): TweetList = Nil
+
   /**
    * The following methods are already implemented
    */
@@ -124,9 +134,43 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet =
+      {
+        val leftAcc = left.filterAcc(p, acc)
+        val rightAcc = right.filterAcc(p, leftAcc)
+        if(p(elem)) rightAcc.incl(elem)
+        else rightAcc
+      }
+
+  def union(that: TweetSet): TweetSet =
+    filterAcc(tw => true, that)
+
+  def isEmpty: Boolean = false
+
+  def mostRetweeted: Tweet = {
+    val leftBiggest = if(!left.isEmpty) left.mostRetweeted else elem
+    val rightBiggest = if(!right.isEmpty) right.mostRetweeted else elem
+    val bigger = if(leftBiggest.retweets > rightBiggest.retweets) leftBiggest else rightBiggest
+    if(elem.retweets > bigger.retweets) elem else bigger
+  }
+
+
+  def descendingByRetweet (): TweetList =
+  {
+    descendingByRetweetHelper(this, Nil)
+  }
+
+  def descendingByRetweetHelper (curr: TweetSet, acc: TweetList): TweetList =
+  {
+    if(curr.isEmpty) acc
+    else
+      {
+        val max =  curr.mostRetweeted
+        val bigger = descendingByRetweetHelper(curr.remove(max), acc)
+        new Cons(max, bigger)
+      }
+  }
+
   /**
    * The following methods are already implemented
    */
@@ -187,10 +231,10 @@ object GoogleVsApple {
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+     //lazy val trending: TweetList = ???
   }
 
 object Main extends App {
   // Print the trending tweets
-  GoogleVsApple.trending foreach println
+  /*GoogleVsApple.trending foreach println*/
 }
